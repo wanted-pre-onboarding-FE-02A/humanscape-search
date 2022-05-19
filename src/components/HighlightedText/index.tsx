5973148d72ef72d1824d647a942055f3d6f12276
changkeun <ckdrmsdk@gmail.com>
2022-05-19 17:39:37 +0900

    Merge: highlightedText merge

---
+++ src/components/HighlightedText/index.tsx
@@ -0,0 +1,37 @@
+import { ReactFragment, useCallback, useMemo } from 'react'
+import { useRecoilValue } from 'recoil'
+
+import { inputValue } from '../../recoil/diseaseInfo'
+
+interface HighlightedTextProps {
+  item: IData
+}
+
+interface IData {
+  sickCd: string
+  sickNm: string
+}
+
+function HighlightedText({ item }: HighlightedTextProps) {
+  const highlight = useRecoilValue(inputValue)
+
+  const regex = useMemo(() => {
+    return new RegExp(`(${highlight})`, 'gi')
+  }, [highlight])
+
+  const parts = item.sickNm.split(regex)
+
+  const renderText = useCallback(
+    (arr: string[]): ReactFragment => {
+      return arr.filter(String).map((part, i) => {
+        const key = `splitedText-${i}`
+        return regex.test(part) ? <mark key={key}>{part}</mark> : <span key={key}>{part}</span>
+      })
+    },
+    [regex]
+  )
+
+  return <span>{renderText(parts)}</span>
+}
+
+export default HighlightedText
